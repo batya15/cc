3f1a5119e70df7d08732ee0d1bb595e6b7e95d5a
Batyr <bcharyev@andiks.com>
2015-02-11 03:16:12 +0300

    gulp copy bower main file

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,29 @@
 
 var config = require('./gulp/config.json');
 var gulp = require('gulp');
+var install = require('gulp-install');
+var mainBowerFiles = require('main-bower-files');
+var ifElse = require('gulp-if-else');
+var path = require('path');
+var through = require('through2');
 
 //Удаление папки билда
 require('./gulp/clean')('clean', gulp, [config.path.build], config.path.src);
 //Удаление папки билда и всех зависимостей npm и bower
-require('./gulp/clean')('cleanHard', gulp, [config.path.build, config.path.bower_components_admin], config.path.src);
-
-gulp.task('default', gulp.series('clean', installModules));
-
-
-var install = require('gulp-install');
+require('./gulp/clean')('cleanHard', gulp, [config.path.build, config.path.bower_components_admin, config.path.bower_components_admin_vendor], config.path.src);
+//Скачивание всех зависимостей
 function installModules () {
-    return gulp.src([config.path.src + '/nodejs/package.json', config.path.src + '/static/admin/bower.json'])
+    return gulp.src([config.modules.package, config.modules.admin_bower])
         .pipe(install());
 }
+//Копирование Main Bower файлов
+function copyBower() {
+    return gulp
+        .src(mainBowerFiles({paths: config.path.staticAdmin}))
+        .pipe(gulp.dest(config.path.staticAdmin + '/vendor'));
+}
+
+
+gulp.task('default', gulp.series('clean', copyBower));
+
+
