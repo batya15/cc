b45b2f83edadb740c3e6ed63242a317d7260744c
Batyr Charyev <bcharyev@andiks.com>
2015-02-11 16:16:04 +0300

    bower copy exclude

---
+++ gulp/bower.js
@@ -0,0 +1,34 @@
+"use strict";
+var mainBowerFiles = require('main-bower-files');
+var gulpFilter = require('gulp-filter');
+var config = require('./config.json');
+
+module.exports = function(gulp, paths, src) {
+    //Копирование Main Bower файлов
+    function bowerJs() {
+        var filter = gulpFilter('*.js');
+        return gulp
+            .src(mainBowerFiles({paths: config.path.staticAdmin}))
+            .pipe(filter)
+            .pipe(gulp.dest(config.path.staticAdmin + '/vendor/js'));
+    }
+
+    function bowerCss() {
+        var filter = gulpFilter('*.css');
+        return gulp
+            .src(mainBowerFiles({paths: config.path.staticAdmin}))
+            .pipe(filter)
+            .pipe(gulp.dest(config.path.staticAdmin + '/vendor/css'));
+    }
+
+    function bowerFonts() {
+        var filter = gulpFilter(['*.eot', '*.svg', '*.ttf', '*.woff']);
+        return gulp
+            .src(mainBowerFiles({paths: config.path.staticAdmin}))
+            .pipe(filter)
+            .pipe(gulp.dest(config.path.staticAdmin + '/vendor/fonts'));
+    }
+
+    return gulp.task('bower', gulp.series(bowerJs, bowerCss, bowerFonts));
+
+};
--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,8 @@
 var config = require('./gulp/config.json');
 var gulp = require('gulp');
 var install = require('gulp-install');
-var mainBowerFiles = require('main-bower-files');
-var gulpFilter = require('gulp-filter');
 
+require('./gulp/bower')(gulp);
 //Удаление папки билда
 require('./gulp/clean')('clean', gulp, [config.path.build], config.path.src);
 //Удаление папки билда и всех зависимостей npm и bower
@@ -15,32 +14,7 @@ function installModules () {
     return gulp.src([config.modules.package, config.modules.admin_bower])
         .pipe(install());
 }
-//Копирование Main Bower файлов
-function bowerJs() {
-    var filter = gulpFilter('*.js');
-    return gulp
-        .src(mainBowerFiles({paths: config.path.staticAdmin}))
-        .pipe(filter)
-        .pipe(gulp.dest(config.path.staticAdmin + '/vendor/js'));
-}
 
-function bowerCss() {
-    var filter = gulpFilter('*.css');
-    return gulp
-        .src(mainBowerFiles({paths: config.path.staticAdmin}))
-        .pipe(filter)
-        .pipe(gulp.dest(config.path.staticAdmin + '/vendor/css'));
-}
-
-function bowerFonts() {
-    var filter = gulpFilter(['*.eot', '*.svg', '*.ttf', '*.woff']);
-    return gulp
-        .src(mainBowerFiles({paths: config.path.staticAdmin}))
-        .pipe(filter)
-        .pipe(gulp.dest(config.path.staticAdmin + '/vendor/fonts'));
-}
-
-gulp.task('bower', gulp.series(bowerJs, bowerCss, bowerFonts));
 
 gulp.task('default', gulp.series('clean', installModules, 'bower'));
 
